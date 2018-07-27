a7cf5512d7568c1674e9c964c79ba1f4bde52fb8
Tanzim Mokammel <mtanzim@gmail.com>
2018-07-27 12:08:44 +0800

    started calling external api

--- routes/api.js
+++ routes/api.js
@@ -8,8 +8,23 @@
 
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
+const axios = require('axios');
+
+const ALPHAKEY = process.env.ALPHAKEY;
+const ALPHAFUNC ='TIME_SERIES_INTRADAY';
+const ALPHASIZE ='compact';
+const ALPHAINTERVAL ='1min';
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+
+function getLatestStock (data) {
+  // return data;
+  let lastTS = data['Meta Data'];
+  console.log(lastTS);
+  let latestResult = data['Time Series (1min)'][lastTS];
+  console.log(latestResult);
+  return latestResult;
+}
 
 module.exports = function (app) {
 
@@ -21,10 +36,30 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res, next){
-      return next(new Error('Not yet developed'));
-    });
-  app.route('/api/stock')
-    .get(function (req, res, next){
+
+      console.log(req.query || 'not sent');
+      if (req.query.stock) {  
+        if (Array.isArray(req.query.stock)){
+          console.log('2 stocks sent');
+          req.query.stock.forEach(stock => {
+            console.log(stock);
+          });
+        } else {
+          console.log('1 stock sent');
+          console.log(req.query.stock);
+          axios.get(`https://www.alphavantage.co/query?function=${ALPHAFUNC}&outputsize=${ALPHASIZE}&symbol=${req.query.stock}&interval=${ALPHAINTERVAL}&apikey=${ALPHAKEY}`)
+            .then(function (response) { 
+              // console.log(response.data);
+              console.log(getLatestStock(response.data));
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        }
+      } else {
+        return res.status(404);
+      }
+
       return next(new Error('Not yet developed'));
     });
     
